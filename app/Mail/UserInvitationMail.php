51c11bb296fd8ca700dcc23eeba40ccbd9945765
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-09 13:50:32 +0700

    Added mailing function for user invitation

---
+++ app/Mail/UserInvitationMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ForgotPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $token;
+    public $email;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($token, $email)
+    {
+        $this->token = $token;
+        $this->email = $email;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        // Build the reset URL that will be used in the email template
+        $resetUrl = route('password.reset', ['token' => $this->token, 'email' => $this->email]);
+
+        return $this->subject('Reset Your Password')
+            ->view('emails.forgot-password', ['resetUrl' => $resetUrl]);
+    }
+}
