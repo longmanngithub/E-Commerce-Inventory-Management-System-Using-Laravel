d176f0bd9a89fc503988cfe79369b1d7cd0c2088
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-09 13:44:49 +0700

    Changed from the default Breeze auth route to own custom route

--- routes/auth.php
+++ routes/auth.php
@@ -7,60 +7,29 @@ use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredCompanyAdminController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\CompanyAdminLoginController;
+use App\Http\Controllers\Auth\CompanyAdminRegisterController;
+use App\Http\Controllers\Auth\CompanyAdminPasswordResetController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+Route::middleware('guest:company_admin,company_staff,platform_owner')->group(function () {
+    // Registration
+    Route::get('register', [CompanyAdminRegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('register', [CompanyAdminRegisterController::class, 'register'])->name('register.attempt');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Login
+    Route::get('login', [CompanyAdminLoginController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [CompanyAdminLoginController::class, 'login'])->name('login.attempt');
 
-    // Register company admin
-    Route::get('register-company-admin', [RegisteredCompanyAdminController::class, 'create'])
-        ->name('register-company-admin');
-
-    Route::post('register', [RegisteredCompanyAdminController::class, 'store']);
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name('password.request');
-
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
+    // Forgot Password
+    Route::get('forgot-password', [CompanyAdminPasswordResetController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('forgot-password', [CompanyAdminPasswordResetController::class, 'sendVerificationCode'])->name('password.email');
+    Route::get('/reset-password', [CompanyAdminPasswordResetController::class, 'showResetForm'])->name('password.reset');
+    Route::post('reset-password', [CompanyAdminPasswordResetController::class, 'reset'])->name('password.update');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-        ->name('verification.notice');
-
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
-
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
-
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
-
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->name('logout');
-});
+// --- LOGOUT ROUTES ---
+// Logout for Company Admin or Staff
+Route::middleware('auth:company_admin,company_staff')->post('logout', [CompanyAdminLoginController::class, 'logout'])->name('admin.logout');
