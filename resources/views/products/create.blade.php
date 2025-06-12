f86d057a22936c0a56247e105b1086827c979c5a
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-12 23:53:18 +0700

    Modified to add reorder point

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -14,12 +14,15 @@
                         @csrf
 
                         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                            {{-- Product name --}}
                             <div>
                                 <x-input-label for="product_name" :value="__('Product Name')" />
                                 <x-text-input id="product_name" class="block mt-1 w-full" type="text" name="product_name" :value="old('product_name')" required />
                                 <x-input-error :messages="$errors->get('product_name')" class="mt-2" />
                             </div>
 
+                            {{-- Category --}}
                             <div>
                                 <x-input-label for="category_id" :value="__('Category')" />
                                 <select name="category_id" id="category_id" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
@@ -34,53 +37,69 @@
                                 <x-input-error :messages="$errors->get('category_id')" class="mt-2" />
                             </div>
 
+                            {{-- Product SKU --}}
                             <div>
                                 <x-input-label for="product_SKU" :value="__('Product SKU')" />
                                 <x-text-input id="product_SKU" class="block mt-1 w-full" type="text" name="product_SKU" :value="old('product_SKU')" required />
                                 <x-input-error :messages="$errors->get('product_SKU')" class="mt-2" />
                             </div>
 
+                            {{-- Stock quantity --}}
                             <div>
                                 <x-input-label for="stock_quantity" :value="__('Amount (Initial Stock)')" />
                                 <x-text-input id="stock_quantity" class="block mt-1 w-full" type="number" name="stock_quantity" :value="old('stock_quantity')" required />
                                 <x-input-error :messages="$errors->get('stock_quantity')" class="mt-2" />
                             </div>
 
+                            {{-- Reorder point --}}
+                            <div>
+                                <x-input-label for="reorder_point" :value="__('Reorder Point (e.g., 10)')" />
+                                <x-text-input id="reorder_point" class="block mt-1 w-full" type="number" name="reorder_point" :value="old('reorder_point', 10)" required />
+                                <x-input-error :messages="$errors->get('reorder_point')" class="mt-2" />
+                            </div>
+
+                            {{-- Purchase price --}}
                             <div>
                                 <x-input-label for="purchase_price" :value="__('Purchase Price (Cost per item)')" />
                                 <x-text-input id="purchase_price" name="purchase_price" type="number" step="0.01" class="block mt-1 w-full" required />
                                 <x-input-error :messages="$errors->get('purchase_price')" class="mt-2" />
                             </div>
 
+                            {{-- Selling price --}}
                             <div>
                                 <x-input-label for="product_price" :value="__('Price')" />
                                 <x-text-input id="product_price" class="block mt-1 w-full" type="number" step="0.01" name="product_price" :value="old('product_price')" required />
                                 <x-input-error :messages="$errors->get('product_price')" class="mt-2" />
                             </div>
 
+                            {{-- Purchase date --}}
                             <div>
                                 <x-input-label for="purchase_date" :value="__('Date of Purchased')" />
                                 <x-text-input id="purchase_date" class="block mt-1 w-full" type="date" name="purchase_date" :value="old('purchase_date')" required />
                                 <x-input-error :messages="$errors->get('purchase_date')" class="mt-2" />
                             </div>
 
-                            <div >
+                            {{-- Product expiry date --}}
+                            <div>
                                 <x-input-label for="product_expiry_date" :value="__('Expiry Date')" />
                                 <x-text-input id="product_expiry_date" class="block mt-1 w-full" type="date" name="product_expiry_date" :value="old('product_expiry_date')" />
                                 <x-input-error :messages="$errors->get('product_expiry_date')" class="mt-2" />
                             </div>
 
+                            {{-- Product desc --}}
                             <div class="md:col-span-2">
                                 <x-input-label for="product_desc" :value="__('Description')" />
                                 <textarea id="product_desc" name="product_desc" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">{{ old('product_desc') }}</textarea>
                                 <x-input-error :messages="$errors->get('product_desc')" class="mt-2" />
                             </div>
 
+                            {{-- Product image --}}
                             <div class="md:col-span-2">
                                 <x-input-label for="product_image" :value="__('Product Image')" />
                                 <input id="product_image" name="product_image" type="file" class="block mt-1 w-full">
                                 <x-input-error :messages="$errors->get('product_image')" class="mt-2" />
                             </div>
+
                         </div>
 
                         <div class="flex items-center justify-end mt-6">
