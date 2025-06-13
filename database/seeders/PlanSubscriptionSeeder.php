362f4d51354ab00faf06e24cf474f549757183ff
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-13 08:13:31 +0700

    Added seeder

---
+++ database/seeders/PlanSubscriptionSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PlanSubscription;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PlanSubscriptionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $plans = [
+            [
+                'subscription_tier' => 'Basic',
+                'subscription_price' => 49.00,
+                'monthly' => 1
+            ],
+            [
+                'subscription_tier' => 'Pro',
+                'subscription_price' => 99.00,
+                'monthly' => 1
+            ],
+            [
+                'subscription_tier' => 'Ultimate',
+                'subscription_price' => 129.00,
+                'monthly' => 1
+            ],
+        ];
+
+        foreach ($plans as $plan) {
+            PlanSubscription::updateOrCreate(['subscription_tier' => $plan['subscription_tier']], $plan);
+        }
+    }
+}
