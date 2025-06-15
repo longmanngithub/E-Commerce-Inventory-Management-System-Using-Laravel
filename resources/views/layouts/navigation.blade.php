409b888b0b7a110b1da7fd5f131037fe6f2d9ee4
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-15 10:08:06 +0700

    Modified navigation link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -60,7 +60,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div>{{ Auth::user()->admin_name ?? Auth::user()->staff_name }}</div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -71,20 +71,17 @@
                     </x-slot>
 
                     <x-slot name="content">
-{{--                        <x-dropdown-link :href="route('profile.edit')">--}}
-{{--                            {{ __('Profile') }}--}}
-{{--                        </x-dropdown-link>--}}
 
                         {{-- This checks which user is logged in and generates the correct route --}}
-                        <x-dropdown-link :href="Auth::guard('platform_owner')->check() ? route('platform_owner.profile.edit') : route('admin.profile.edit')">
+                        <x-dropdown-link :href="route('admin.profile.edit')">
                             {{ __('Account Settings') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
-                        <form method="POST" action="{{ Auth::guard('platform_owner')->check() ? route('owner.logout') : route('logout') }}">
+                        <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="Auth::guard('platform_owner')->check() ? route('owner.logout') : route('logout')"
+                            <x-dropdown-link :href="route('logout')"
                                              onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
@@ -123,15 +120,15 @@
             </div>
 
             <div class="mt-3 space-y-1">
-{{--                <x-responsive-nav-link :href="route('profile.edit')">--}}
-{{--                    {{ __('Profile') }}--}}
-{{--                </x-responsive-nav-link>--}}
+                <x-responsive-nav-link :href="route('admin.profile.edit')">
+                    {{ __('Profile') }}
+                </x-responsive-nav-link>
 
                 <!-- Authentication -->
-                <form method="POST" action="{{ Auth::guard('platform_owner')->check() ? route('owner.logout') : route('logout') }}">
+                <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-dropdown-link :href="Auth::guard('platform_owner')->check() ? route('owner.logout') : route('logout')"
+                    <x-dropdown-link :href="route('logout')"
                                      onclick="event.preventDefault();
                         this.closest('form').submit();">
                         {{ __('Log Out') }}
