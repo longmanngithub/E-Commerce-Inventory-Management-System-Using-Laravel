df98bfe0924a4ace528c0c255dc8d29c3a0a320e
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-17 08:42:30 +0700

    Modified auth view

--- resources/views/auth/set-password.blade.php
+++ resources/views/auth/set-password.blade.php
@@ -7,39 +7,30 @@
     <form method="POST" action="{{ route('invitation.store_password') }}">
         @csrf
 
-        <input type="hidden" name="token" value="{{ $invitation->token }}">
+        {{-- Hidden fields to pass the token and email to the next request --}}
+        <input type="hidden" name="token" value="{{ $invitationData['token'] }}">
+        <input type="hidden" name="email" value="{{ $invitationData['email'] }}">
 
-        <input type="hidden" name="name" value="{{ $invitation->name }}">
-
-        <div>
+        {{-- Email Address (disabled for user) --}}
+        <div class="mt-4">
             <x-input-label for="email" :value="__('Email Address')" />
             <x-text-input id="email" class="block mt-1 w-full bg-gray-100"
                           type="email"
-                          name="email"
-                          :value="$invitation->email"
-                          required
+                          :value="$invitationData['email']"
                           disabled />
         </div>
 
+        {{-- Password Input --}}
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full"
-                          type="password"
-                          name="password"
-                          required
-                          autofocus
-                          autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autofocus />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
+        {{-- Confirm Password Input --}}
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                          type="password"
-                          name="password_confirmation"
-                          required
-                          autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required />
         </div>
 
         <div class="flex items-center justify-end mt-4">
