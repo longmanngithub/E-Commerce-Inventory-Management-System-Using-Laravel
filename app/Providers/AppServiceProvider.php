f6e29be103721eecfcdd2da7e42c8cd510ffee60
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-18 11:40:35 +0700

    Modified to disable the ProductObserver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Product::observe(ProductObserver::class);
+//        Product::observe(ProductObserver::class);
     }
 }
