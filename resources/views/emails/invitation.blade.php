d336a4d9804eeac27a78fed40907b80155ccc9aa
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-18 15:17:12 +0700

    Added view for mail

---
+++ resources/views/emails/invitation.blade.php
@@ -0,0 +1,4 @@
+<h1>You're Invited!</h1>
+
+<p>You have been invited to join the company. Click the link below to create your account and set your password:</p>
+<a href="{{ $invitationUrl }}">{{ $invitationUrl }}</a>
