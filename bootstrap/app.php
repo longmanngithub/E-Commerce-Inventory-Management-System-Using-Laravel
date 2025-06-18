b1437900e182b32a6b0519dd96d720aec87c2e0a
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-18 15:12:29 +0700

    Modified bootstrap/app config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +14,29 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // This configures where guests should be redirected.
+        $middleware->redirectGuestsTo(function (Request $request) {
+            // If the request is for an API route...
+            if ($request->is('api/*')) {
+                // ...do not redirect at all. This will allow the exception
+                // handler to return a proper JSON error response.
+                return null;
+            }
+        });
+        $middleware->alias([
+            'company.valid' => \App\Http\Middleware\CheckCompanyIsValid::class,
+        ]);
     })
+    ->withProviders([
+        // This registers our new provider so Laravel will load it
+        App\Providers\AuthServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // This tells Laravel how to handle authentication errors for API requests.
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->expectsJson()) {
+                return response()->json(['message' => 'Unauthenticated.'], 401);
+            }
+        });
     })->create();
