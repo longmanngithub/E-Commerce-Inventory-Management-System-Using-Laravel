75749708cd8118f353f88c3ce5c52c7f4dc109d2
longmanngithub <69972928+longmanngithub@users.noreply.github.com>
2025-06-18 15:16:35 +0700

    Added notification

---
+++ app/Notifications/LowStockWarning.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Product;
+
+class LowStockWarning extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public Product $product)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'product_id' => $this->product->product_id,
+            'product_name' => $this->product->product_name,
+            'message' => "Warning: Stock for '{$this->product->product_name}' is low.",
+            'link' => '/products/' . $this->product->product_id, // Link to the product detail page
+        ];
+    }
+}
